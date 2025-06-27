c8795a891fefb8028282c38d8bf09ef18d0c48cb
Guilherme de Moura Oliveira <guimoura.oliveira@gmail.com>
2025-06-26 21:53:55 -0300

    fix some items

--- src/http/controllers/auth/sign-out.controller.ts
+++ src/http/controllers/auth/sign-out.controller.ts
@@ -1,6 +1,6 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
-import { env } from '../../../utils/libs/env';
+import { FastifyReply, FastifyRequest } from 'fastify';
 import { StatusCodes } from 'http-status-codes';
+import { env } from '../../../utils/libs/env';
 import logger from '../../../utils/libs/logger';
 
 const filename = __filename.split(/[/\\]/).pop();
@@ -9,7 +9,7 @@ export async function signOut(request: FastifyRequest, reply: FastifyReply) {
     logger.info(`${filename} -> Initiating sign-out process...`);
 
     try {
-        await request.jwtVerify({ onlyCookie: true });
+        await request.jwtVerify({ onlyCookie: true, ignoreExpiration: true });
         logger.info(`${filename} -> JWT verification successful. Clearing refresh token.`);
 
         return reply.clearCookie(env.REFRESH_TOKEN_NAME).status(StatusCodes.OK).send({});
